42cd34013760acfc63a749ce80a04ce5e66f384d
Dmytro_Plekhotkin <dmytro_plekhotkin@epam.com>
2016-01-22 12:07:10 +0200

    Commit the program

---
+++ src/dmytro/Application.java
@@ -0,0 +1,35 @@
+package dmytro;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class Application {
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		if (args.length != 1) {
+			System.out.println("Program should have 1 parameter: path to folder which you want to delete");
+		} else {
+			Path directory = Paths.get(args[0]);
+			Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					Files.delete(file);
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+					Files.delete(dir);
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+		}
+	}
+}
